c08f73304edfa9c0a6c16c20a071dba4ba10d7d1
Kevin Sun <kevsun1@gmail.com>
2016-05-18 17:53:28 -0400

    rename functions: From -> For

--- lib/transfermarkt.py
+++ lib/transfermarkt.py
@@ -9,28 +9,28 @@ useragent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)
 # "lol" can be anything - transfermarkt ignores it/uses it for semantic url
 urlprefix = 'http://www.transfermarkt.com/lol/'
 
-def getUrlFromLeagueId(id, season=2015):
+def getUrlForLeagueId(id, season=2015):
     return urlprefix + 'startseite/wettbewerb/' + id + '/saison_id/' + str(season)
 
-def getUrlFromClubId(id, season=2015):
+def getUrlForClubId(id, season=2015):
     return urlprefix + 'startseite/verein/' + str(id) + '/saison_id/' + str(season)
 
-def getUrlFromPlayerId(id):
+def getUrlForPlayerId(id):
     return urlprefix + 'profil/spieler/' + str(id)
 
-def getClubsFromLeagueId(id, season=2015):
-    bs = BeautifulSoup(urlopen(Request(getUrlFromLeagueId(id, season), headers={'User-Agent': useragent})))
+def getClubsForLeagueId(id, season=2015):
+    bs = BeautifulSoup(urlopen(Request(getUrlForLeagueId(id, season), headers={'User-Agent': useragent})))
     elements = bs.find(id='yw1').find_all("td",class_="hauptlink no-border-links hide-for-small hide-for-pad")
     return [{'clubId': e.find("a")["id"], 'name': e.getText()} for e in elements]
 
-def getPlayersFromClubId(id, season=2015):
-    bs = BeautifulSoup(urlopen(Request(getUrlFromClubId(id, season), headers={'User-Agent': useragent})))
+def getPlayersForClubId(id, season=2015):
+    bs = BeautifulSoup(urlopen(Request(getUrlForClubId(id, season), headers={'User-Agent': useragent})))
     elements = bs.find(id='yw1').find_all("span",class_="hide-for-small")
     return [{'playerId': e.find("a", class_="spielprofil_tooltip")["id"], 
              'name': e.getText()} for e in elements if e.find("a", class_="spielprofil_tooltip")]
 
-def getTransfersFromPlayerId(id):
-    bs = BeautifulSoup(urlopen(Request(getUrlFromPlayerId(id), headers={'User-Agent': useragent})))
+def getTransfersForPlayerId(id):
+    bs = BeautifulSoup(urlopen(Request(getUrlForPlayerId(id), headers={'User-Agent': useragent})))
     elements = bs.find(class_="transferhistorie").find_all("tr",class_="zeile-transfer")
     dicts = [{'seasonDate': "  ".join([td.getText() for td in e.findAll("td")[:2]]),
       'mv': e.find("td",class_="zelle-mw").getText(),
@@ -44,3 +44,5 @@ def getTransfersFromPlayerId(id):
        'fromTeamName': d['teams']['from']['name'].lstrip(),
        'toTeamId': d['teams']['to']['teamId'],
        'toTeamName': d['teams']['to']['name'].lstrip()} for d in dicts]
+
+
