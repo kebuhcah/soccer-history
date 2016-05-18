f94cfa06525d7b58517b59992c0d9ff2fa81d44c
Kevin Sun <kevsun1@gmail.com>
2016-05-18 11:37:00 -0400

    refactor - extract url prefix to variable

--- lib/transfermarkt.py
+++ lib/transfermarkt.py
@@ -5,9 +5,10 @@ from bs4 import BeautifulSoup
 from pandas import DataFrame
 
 useragent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+urlprefix = 'http://www.transfermarkt.com/lol/'
 
 def getUrlFromLeagueId(id, season=2015):
-    return 'http://www.transfermarkt.com/lol/startseite/wettbewerb/' + id + '/saison_id/' + str(season)
+    return urlprefix + 'startseite/wettbewerb/' + id + '/saison_id/' + str(season)
 
 def getClubsFromLeagueId(id, season=2015):
     bs = BeautifulSoup(urlopen(Request(getUrlFromLeagueId(id, season), headers={'User-Agent': useragent})))
@@ -15,13 +16,13 @@ def getClubsFromLeagueId(id, season=2015):
     return DataFrame([{'id': cp.find("a")["id"], 'name': cp.getText(), 'url': cp.find("a")["href"]} for cp in elements])
 
 def getUrlFromClubId(id, season=2015):
-    return 'http://www.transfermarkt.com/lol/startseite/verein/' + str(id) + '/saison_id/' + str(season)
+    return urlprefix + 'startseite/verein/' + str(id) + '/saison_id/' + str(season)
 
 def getPlayersFromClubId(id, season=2015):
     bs = BeautifulSoup(urlopen(Request(getUrlFromClubId(418), headers={'User-Agent': useragent})))
     elements = bs.find(id='yw1').find_all("span",class_="hide-for-small")
-    return DataFrame([{'id': cp.find("a", class_="spielprofil_tooltip")["id"], 'name': cp.getText(), 'url': cp.find("a")["href"]} for cp in elements
+    return DataFrame([{'id': cp.find("a", class_="spielprofil_tooltip")["id"], 'name': cp.getText(), 'url': cp.find("a")["href"]} for cp in elements 
                       if cp.find("a", class_="spielprofil_tooltip")])
 
 def getUrlFromPlayerId(id):
-    return 'http://www.transfermarkt.com/lol/profil/spieler/' + str(id)
+    return urlprefix + 'profil/spieler/' + str(id)
