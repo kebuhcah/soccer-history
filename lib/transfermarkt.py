3f5412128e70dfde70fb6977f07a50d77fd584cd
Kevin Sun <kevsun1@gmail.com>
2016-05-18 11:58:34 -0400

    bug fix: clubId was always 418 for getPlayersFromClubId

--- lib/transfermarkt.py
+++ lib/transfermarkt.py
@@ -12,19 +12,19 @@ urlprefix = 'http://www.transfermarkt.com/lol/'
 def getUrlFromLeagueId(id, season=2015):
     return urlprefix + 'startseite/wettbewerb/' + id + '/saison_id/' + str(season)
 
+def getUrlFromClubId(id, season=2015):
+    return urlprefix + 'startseite/verein/' + str(id) + '/saison_id/' + str(season)
+
+def getUrlFromPlayerId(id):
+    return urlprefix + 'profil/spieler/' + str(id)
+
 def getClubsFromLeagueId(id, season=2015):
     bs = BeautifulSoup(urlopen(Request(getUrlFromLeagueId(id, season), headers={'User-Agent': useragent})))
     elements = bs.find(id='yw1').find_all("td",class_="hauptlink no-border-links hide-for-small hide-for-pad")
     return [{'clubId': cp.find("a")["id"], 'name': cp.getText()} for cp in elements]
 
-def getUrlFromClubId(id, season=2015):
-    return urlprefix + 'startseite/verein/' + str(id) + '/saison_id/' + str(season)
-
 def getPlayersFromClubId(id, season=2015):
-    bs = BeautifulSoup(urlopen(Request(getUrlFromClubId(418), headers={'User-Agent': useragent})))
+    bs = BeautifulSoup(urlopen(Request(getUrlFromClubId(id), headers={'User-Agent': useragent})))
     elements = bs.find(id='yw1').find_all("span",class_="hide-for-small")
-    return [{'playerId': cp.find("a", class_="spielprofil_tooltip")["id"], 'name': cp.getText()} for cp in elements
-                      if cp.find("a", class_="spielprofil_tooltip")]
-
-def getUrlFromPlayerId(id):
-    return urlprefix + 'profil/spieler/' + str(id)
+    return [{'playerId': cp.find("a", class_="spielprofil_tooltip")["id"], 
+             'name': cp.getText()} for cp in elements if cp.find("a", class_="spielprofil_tooltip")]
