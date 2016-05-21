33baab9047c10ee41ef75b0b10bf641d07906aea
Kevin Sun <kevsun1@gmail.com>
2016-05-21 17:09:02 -0400

    better handling of social media typos

--- lib/transfermarkt.py
+++ lib/transfermarkt.py
@@ -6,8 +6,8 @@ from pandas import DataFrame
 import pandas as pd
 import numpy as np
 import re
-import os.path
-import datetime
+import os.path 
+import datetime 
 
 # transfermarkt blocks default useragent
 useragent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
@@ -37,7 +37,7 @@ def getClubsByLeagueId(id, season=2015):
 def getPlayersByClubId(id, season=2015):
     bs = BeautifulSoup(urlopen(Request(getUrlByClubId(id, season), headers={'User-Agent': useragent})))
     elements = bs.find(id='yw1').find_all("span",class_="hide-for-small")
-    return [{'playerId': e.find("a", class_="spielprofil_tooltip")["id"],
+    return [{'playerId': e.find("a", class_="spielprofil_tooltip")["id"], 
              'name': e.getText()} for e in elements if e.find("a", class_="spielprofil_tooltip")]
 
 def getTransfersFromBs(bs):
@@ -61,18 +61,19 @@ def getTransfersFromBs(bs):
         'playerName': bs.find("h1").getText()} for d in dicts]
 
 def getPlayerDataFromBs(bs):
-    elements = bs.find(class_="spielerdaten").find_all("tr")
-    result1 = [{'key': e.find("th").getText().strip().rstrip(':'),'value': e.find("td").getText().strip(),
+    elements = bs.find(class_="spielerdaten").find_all("tr") 
+    result1 = [{'key': e.find("th").getText().strip().rstrip(':'),'value': e.find("td").getText().strip(), 
             'country': (e.find('img',class_="flaggenrahmen").get("title")) if e.find('img',class_="flaggenrahmen") else "",
             'hrefs' : [a.get("href") for a in e.findAll("a")]} for e in elements]
 
-    result2 = [{'key':e['key'], 'value': (e['value']
+    result2 = [{'key':e['key'], 'value': (e['value'] 
            + (" COUNTRY:" + e['country'] if e['key'] == 'Place of birth' else '')
-           + (" HREFs: " + " ".join(e['hrefs']) if len(e['hrefs']) > 0 else '')).strip()
+           + (" HREFs: " + " ".join(e['hrefs']) if len(e['hrefs']) > 0 else '')
+           + ("SOCMED: " + "####".join(e['hrefs']) if len(e['hrefs']) > 0 else '')).strip()
           } for e in result1]
 
     result3 = dict([(e['key'],e['value']) for e in result2])
-
+    
     result3["Display name"] = bs.find("h1").getText()
     print "now processing " + result3["Display name"].encode('utf-8')
     if bs.find("div",class_="detailpositionen"):
@@ -103,8 +104,8 @@ def getPlayerDataFromBs(bs):
     if "2nd club" in result3:
         result3["2nd club id"] = result3["2nd club"].split("/verein/")[-1].strip()
         result3["2nd club"] = result3["2nd club"].split("HREFs:")[0].strip()
-    if "Social media" in result3:
-        socialmedia = dict([(url.split("//")[-1].split("/")[0].split(".")[-2], url) for url in result3["Social media"].split(" ") if url.startswith("http")])
+    if "Social media" in result3:    
+        socialmedia = dict([(url.split("://")[-1].split("/")[0].split(".")[-2].strip(), url) for url in result3["Social media"].split("SOCMED: ")[-1].split("####") if url.startswith("http")])    
         for platform in ['twitter', 'facebook', 'instagram']:
             if platform in socialmedia:
                 result3[platform]=socialmedia[platform]
@@ -115,5 +116,5 @@ def getPlayerDataFromBs(bs):
     if nationalTeamLinks:
         result3['Intl caps/goals'] = '/'.join([a.getText() for a in nationalTeamLinks])
         result3['National team id'] = nationalTeamLinks[0].get("href").split("/")[-1]
-
+    
     return result3
